566d9ec049e9ae4c44012b55ecc348d6c1f83da3
Nana Kwame <105567071+chiro999@users.noreply.github.com>
2023-08-06 20:19:48 +0000

    Add files via upload

---
+++ Import_Processing.m
@@ -0,0 +1,21 @@
+I=imread('pout.tif');
+
+imshow(I)
+
+whos I
+
+figure
+
+imhist(I)
+
+
+I2 = histeq(I);
+figure
+imshow(I2)
+
+figure
+imhist(I2)
+
+imwrite (I2, 'pout2.png');
+
+imfinfo('pout2.png')
